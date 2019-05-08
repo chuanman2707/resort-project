08e220efd3b5b2eee0ea971e019f3b8d7dea995a
chuanman2707 <chuanman2707@gmail.com>
2019-05-08 16:46:00 +0700

    update again

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -30,16 +30,45 @@ export default class SingleRoom extends Component {
       )
     }
     const { name, description, capacity, size, price, extras, breakfast, pets, images } = room;
-
+    const [mainImg, ...defaultImg] = images
     return (
       <>
-      <StyledHero img={images[0] || this.state.defaultBcg} >
+      <StyledHero img={mainImg || this.state.defaultBcg} >
         <Banner title={`${name} room`}>
         <Link to="/rooms" className="btn-primary">
         Back To Rooms
         </Link>
         </Banner>
       </StyledHero>
+      <section className="single-room">
+        <div className="single-room-images">
+          {defaultImg.map((item, index) =>{
+            return<img key={index} src={item} alt={name} />
+          })}
+        </div>
+        <div className="single-room-info">
+          <article className="desc">
+            <h3>Details</h3>
+            <p>{description}</p>
+          </article>
+          <article className="info">
+            <h3>Info</h3>
+            <h6>Price : ${price}</h6>
+            <h6>Size: ${size} square meter</h6>
+            <h6> MaxCapacity : {" "} {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
+            <h6>{pets?"Pet Allowed":"No Pet Allowed"}</h6>
+            <h6>{breakfast && "No Breakfast included"}</h6>
+          </article>
+        </div>
+      </section>
+      <section className="room-extras">
+        <h6>Extras</h6>
+        <ul className="extras">
+          {extras.map((item, index) =>
+          <li key={index}>- {item}</li>
+          )}
+        </ul>
+      </section>
       </>
     )
   }
