2db446c0a96e8d30bebe39442c4da9bdadb8229c
chuanman2707 <chuanman2707@gmail.com>
2019-05-09 19:32:59 +0700

    fix env

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,8 +1,8 @@
 import {createClient} from 'contentful';
 
  export default createClient({
-    space:process.env.ACCESS_SPACE,
-    accessToken:process.env.ACCESS_TOKEN
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
 
 console.log(process.env.ACCESS_SPACE)
